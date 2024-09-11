270e449cfb9be37f741046939c3f91e447ad8376
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2024-09-11 15:04:28 +0700

    uploading last exercises

--- Exercises/Set 07/Q5.ts
+++ Exercises/Set 07/Q5.ts
@@ -42,3 +42,4 @@ console.log(rfact(7));  // '7! = 7 x 6 x 5 x 4 x 3 x 2 x 1 = 5040'
 
 console.log(rfact(25));
 // '25! = 25 x 24 x 23 x 22 x 21 x 20 x 19 x 18 x 17 x 16 x 15 x 14 x 13 x 12 x 11 x 10 x 9 x 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1 = 1.5511210043330984e+25'
+
